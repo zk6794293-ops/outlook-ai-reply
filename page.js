14eda132d7d98cc3c8bc2cb1c131be02d6b60ef5
zk6794293-ops <zk6794293@gmail.com>
2026-07-07 19:33:13 +0500

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div style={{padding: '50px', textAlign: 'center'}}>
-      <h1>Root page.js Working ✅</h1>
-      <p>اگر یہ کھل گیا تو app/page.js Vercel ignore کر رہا ہے</p>
-    </div>
-  )
-}
